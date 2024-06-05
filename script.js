93d9abb966d28f72185f56038c149d92b5a05213
YASIRPATHAN <yasirameen631@gmail.com>
2024-06-05 12:44:58 +0400

    Web Development

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('videos1').addEventListener('click', function() {
+    const videoPlayer = document.getElementById('videoPlayer1');
+    videoPlayer.src = 'PUBG MOBILE Global Launch Trailer.mp4';
+    videoPlayer.play();
+});
+
+
+
